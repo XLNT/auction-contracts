55bfeb956c9d0bdf4564feb81fdf2e0cd828d4bc
Feridun Mert Celebi <mertcelebi93@gmail.com>
2018-02-13 10:31:19 -0800

    Remove unnecessary note

--- contracts/AuctionBase.sol
+++ contracts/AuctionBase.sol
@@ -311,11 +311,6 @@ contract AuctionBase is Pausable, IAssetHolder, EIP820Implementer {
   /// @dev Computes auction house's cut of a sale.
   /// @param _price - highestBid for the NFT.
   function _computeAuctionHouseCut(uint256 _price) internal view returns (uint256) {
-      // NOTE: We don't use SafeMath (or similar) in this function because
-      //  all of our entry functions carefully cap the maximum values for
-      //  currency (at 128-bits), and ownerCut <= 10000 (see the require()
-      //  statement in the ClockAuction constructor). The result of this
-      //  function is always guaranteed to be <= _price.
       return _price.mul(auctionHouseCut).div(10000);
   }
 
